518ac72beea009980fb4cc86dc4a31834e2d2cc9
Chelsea Urquhart <chelsea.urquhart@rocketsimplicity.com>
2018-10-27 01:23:12 -0700

    allow IFunction to throw exceptions

--- src/main/java/com/chelseaurquhart/securejson/IFunction.java
+++ src/main/java/com/chelseaurquhart/securejson/IFunction.java
@@ -5,5 +5,5 @@ interface IFunction<T, R> {
      * Performs this operation on the given argument and returns the result.
      * @param parInput The input argument.
      */
-    R accept(T parInput);
+    R accept(T parInput) throws Exception;
 }
--- src/main/java/com/chelseaurquhart/securejson/IObjectMutator.java
+++ src/main/java/com/chelseaurquhart/securejson/IObjectMutator.java
@@ -6,5 +6,5 @@ interface IObjectMutator extends IFunction<Object, Object> {
      * @param parInput The input argument.
      * @return The processed object.
      */
-    Object accept(Object parInput);
+    Object accept(Object parInput) throws JSONException;
 }
